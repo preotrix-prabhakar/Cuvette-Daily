9514e6d483a16ee7c8fd540062527daaaeb4222a
preotrix-prabhakar <prabhakar8859@gmail.com>
2024-07-12 21:41:30 +0530

    add cards

--- 9july/API/script.js
+++ 9july/API/script.js
@@ -1,25 +1,22 @@
-let product1=document.getElementsByClassName('product-list2')[0]
 let products=document.getElementById('product-list')
 
 fetch('https://fakestoreapi.com/products').then((response)=>{
     response.json().then((data)=>{
        data.forEach(item => {
-        let title=document.createElement('li');
-        title.innerHTML=item.title;
+        let title=document.createElement('div');
+        title.innerHTML=`<div class="card" style="width: 18rem;">
+        <img class="card-img-top" src=" ${item.image}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+          <p class="card-text">price is ${item.price}.</p>
+          <a href="#" class="btn btn-primary">Add to cart</a>
+        </div>
+      </div>
+      `;
         products.appendChild(title);
        });
     });
 }).catch((e)=>{
     console.log('something went wrong');
 })
-fetch('https://fakestoreapi.com/products').then((response)=>{
-    response.json().then((data)=>{
-       data.forEach(item => {
-        let title=document.createElement('li');
-        title.innerHTML=item.title;
-        product1.appendChild(title);
-       });
-    });
-}).catch((e)=>{
-    console.log('something went wrong');
-})
+
